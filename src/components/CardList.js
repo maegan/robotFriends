1d4a31dc78ccd35e3bb0cf5ec736fa32b89ebe91
maegan <maegan@siena.edu>
2024-02-13 17:03:59 -0500

    testing errorboundary

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
-
-const CardList = ({robots}) => {
-  return (
-    <div>
-    {
-      robots.map((user, i) => {
-      return (
-        <Card
-          key={robots[i].id}
-          id={robots[i].id}
-          name={robots[i].name}
-          email={robots[i].email}
-          />
-        );
-      })
-    }
-    </div>
-  );
-}
+const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error("NOOOOO");
+  } else {
+    return (
+      <div>
+        {robots.map((user, i) => {
+          return (
+            <Card
+              key={robots[i].id}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+};
 
 export default CardList;
